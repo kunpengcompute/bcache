156caab280ab93340ed507f2151fbcd0613d2a81
bensongchao1989 <sungchao@126.com>
2021-04-02 10:06:05 +0800

    bcache: add separate workqueue for journal_write to avoid deadlock

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -70,6 +70,7 @@ static int bcache_major;
 static DEFINE_IDA(bcache_minor);
 static wait_queue_head_t unregister_wait;
 struct workqueue_struct *bcache_wq;
+struct workqueue_struct *bch_journal_wq;
 
 #define BTREE_MAX_PAGES		(256 * 1024 / PAGE_SIZE)
 /* limitation of partitions number on single bcache device */
@@ -2135,6 +2136,9 @@ static void bcache_exit(void)
 		kobject_put(bcache_kobj);
 	if (bcache_wq)
 		destroy_workqueue(bcache_wq);
+	if (bch_journal_wq)
+		destroy_workqueue(bch_journal_wq);
+
 	if (bcache_major)
 		unregister_blkdev(bcache_major, "bcache");
 	acache_dev_exit();
@@ -2162,6 +2166,7 @@ static int __init bcache_init(void)
 	}
 
 	if (!(bcache_wq = alloc_workqueue("bcache", WQ_MEM_RECLAIM, 0)) ||
+	    !(bch_journal_wq = alloc_workqueue("bch_journal", WQ_MEM_RECLAIM, 0)) ||
 	    !(bcache_kobj = kobject_create_and_add("bcache", fs_kobj)) ||
 	    sysfs_create_files(bcache_kobj, files) ||
 	    bch_request_init() ||
