67f942315f9b645f0095e34c9e5365c1ca172291
bensongchao1989 <sungchao@126.com>
2021-04-08 09:53:10 +0800

    bcache: explicitly destroy mutex while exiting

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -2139,6 +2139,7 @@ static void bcache_exit(void)
 		unregister_blkdev(bcache_major, "bcache");
 	acache_dev_exit();
 	unregister_reboot_notifier(&reboot);
+	mutex_destroy(&bch_register_lock);
 }
 
 static int __init bcache_init(void)
@@ -2158,15 +2159,16 @@ static int __init bcache_init(void)
 	bcache_major = register_blkdev(0, "bcache");
 	if (bcache_major < 0) {
 		unregister_reboot_notifier(&reboot);
+		mutex_destroy(&bch_register_lock);
 		return bcache_major;
 	}
 
 	if (!(bcache_wq = alloc_workqueue("bcache", WQ_MEM_RECLAIM, 0)) ||
 	    !(bch_journal_wq = alloc_workqueue("bch_journal", WQ_MEM_RECLAIM, 0)) ||
 	    !(bcache_kobj = kobject_create_and_add("bcache", fs_kobj)) ||
-	    sysfs_create_files(bcache_kobj, files) ||
 	    bch_request_init() ||
-	    bch_debug_init(bcache_kobj))
+	    bch_debug_init(bcache_kobj) ||
+	    sysfs_create_files(bcache_kobj, files))
 		goto err;
 
 	ret = acache_dev_init();
